a6abe2de5e35c8934c0c606540b3f88d7160b3e1
Denis Maslakov <denis.maslakov@sibers.com>
2017-10-02 18:03:30 +0700

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, { history } from './redux/store'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store} >
+      <ConnectedRouter history={history} >
+        <App/>
+      </ConnectedRouter>
+    </Provider>,
+    div);
 });
